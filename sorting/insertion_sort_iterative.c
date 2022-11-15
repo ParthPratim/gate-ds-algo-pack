6ed6b0ae70fd72c2aa73bb1265da07dd34e731a1
Parth Pratim Chatterjee <parth27official@gmail.com>
2022-11-15 20:31:21 +0530

    Insertion Sort (Iterative)

--- sorting/insertion_sort_iterative.c
+++ sorting/insertion_sort_iterative.c
@@ -1,5 +1,23 @@
-#include <stdin>
+#include <stdio.h>
 #include <stdlib.h>
+
+
+void Insertion_Sort(int ** arrx, int n){
+    int * arr = *arrx;
+    for(int i = 0 ; i < n ; i++){
+        int key = arr[i];
+        int j = i-1;
+
+        // skip all elements on right of key which are bigger than it, and place it in it's correct position
+        while(j >= 0 && arr[j] > key){
+            arr[j+1] = arr[j]; // move each element bigger that key to it's right, to make place for the key
+            j--;
+        }
+
+        // Now, j+1 is the correct index of key
+        arr[j+1] = key;
+    }
+}
 
 int getInput(int ** arr){
     int n;
@@ -18,6 +36,8 @@ int getInput(int ** arr){
 int main(){
     int * arr;
     int n = getInput(&arr);
-    
+    Insertion_Sort(&arr,n);
+    for(int i = 0 ; i < n ; i++) printf("%d ", arr[i]);
+    printf("\n");
     return 0;
 }
