203c8a02b66c16a342a23b9fe77e2e463de8e6fc
Parth Pratim Chatterjee <parth27official@gmail.com>
2022-11-15 20:20:33 +0530

    1D Array Allocation

---
+++ pointers/1d_array.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+// A PROGRAM TO CREATE A TWO DIMENSIONAL ARRAY OF SIZE " N " 
+
+int main(){
+
+    int n;
+    scanf("%d",&n);
+    
+    int * one_dim = (int*) malloc(n * sizeof(int));
+
+}
