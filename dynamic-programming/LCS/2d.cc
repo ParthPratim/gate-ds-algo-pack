9ab8e3e3cd36f5cd38769ddeed0acc88105760f3
Parth Pratim Chatterjee <parth27official@gmail.com>
2022-11-15 20:05:54 +0530

    LCS Code

--- dynamic-programming/LCS/2d.cc
+++ dynamic-programming/LCS/2d.cc
@@ -6,5 +6,23 @@ int main(){
     cin >> s >> t;
     int n = s.length();
     int m = t.length();
+
+    vector<vector<int>> dp(n+1, vector<int>(m+1));
+
+    for(int i = n-1; i >= 0 ; i--){
+        for(int j = m-1; j >=0 ; j--){
+            // i is ptr of string s
+            // j is ptr of string t
+            if(s[i] == t[j]){
+                dp[i][j] = dp[i+1][j+1] + 1; 
+            }
+            else{
+                dp[i][j] = max(dp[i+1][j], dp[i][j+1]);
+            }
+        }
+    }
+
+    cout << "Maximum LCS Length : " << dp[0][0] << "\n";
+
     return 0;
 }
