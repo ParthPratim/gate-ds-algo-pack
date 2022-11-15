c06a7ff80ac9b88e6ead9f7995dc8bc1bcdaf78b
Parth Pratim Chatterjee <parth27official@gmail.com>
2022-11-15 21:11:04 +0530

    Aligned to class formula:

--- dynamic-programming/Matrix-Chain/2d_using_p_array.c
+++ dynamic-programming/Matrix-Chain/2d_using_p_array.c
@@ -9,8 +9,8 @@ typedef struct
 
 void Print2DArray(int*** arr , int n){
     int ** arrx = *arr;;
-    for(int i = 0 ; i < n ; i++){
-        for(int j = 0 ; j < n ; j++){
+    for(int i = 1 ; i <= n ; i++){
+        for(int j = 1 ; j <= n ; j++){
             printf("%d ", arrx[i][j]);
         }
         printf("\n");
@@ -24,7 +24,7 @@ int main(){
     scanf("%d",&n);
 
     // Creating a 2D Array of size N X 2 , to store the number of rows and columns for all the n matrices
-    Matrix * dims = (Matrix *) malloc(n * sizeof(Matrix));
+    Matrix * dims = (Matrix *) malloc((n+1) * sizeof(Matrix));
 
     int * p = (int* )malloc((n+1) * sizeof(int));
 
@@ -39,18 +39,18 @@ int main(){
 
     int ** M, ** S;
     // declaring the M-Table and the S-Table.
-
-    M = (int**) malloc(n * sizeof(int**));
-    S = (int**) malloc(n * sizeof(int**));
-    for(int i = 0 ; i < n ; i++){
-        M[i] = (int*) malloc(n * sizeof(int *));
-        S[i] = (int*) malloc(n * sizeof(int *));
+    // n+1 is taken so that we can do 1-based indexing from 1-to-N
+    M = (int**) malloc((n+1) * sizeof(int**));
+    S = (int**) malloc((n+1) * sizeof(int**));
+    for(int i = 0 ; i < n+1 ; i++){
+        M[i] = (int*) malloc((n+1) * sizeof(int *));
+        S[i] = (int*) malloc((n+1) * sizeof(int *));
     }
 
     //since, in Matrix chain multiplication, we fill the table diagonally
-    for(int diagonal = 0 ; diagonal < n ; diagonal++){
-        int i = 0 , j = diagonal;
-        while (i < n && j < n)
+    for(int diagonal = 1 ; diagonal <= n ; diagonal++){
+        int i = 1 , j = diagonal;
+        while (i <= n && j <= n)
         {
             if(i == j){
                 // Multiplying only one matrix takes 0 operations, as multiplication takes two operands
@@ -61,10 +61,7 @@ int main(){
                 int min_value = INT_MAX;
                 int min_k = -1;
                 for(int k = i; k < j ; k++){
-                    // In class, it was given p[i-1]*p[k]*p[j]
-                    // but, since the above in class was taken as 1-based indexing,
-                    // in 0-based indexing : p[i]*p[k+1]*p[j+1]
-                    int val = M[i][k] + M[k+1][j] + (p[i] * p[k+1] * p[j+1]);
+                    int val = M[i][k] + M[k+1][j] + (p[i-1] * p[k] * p[j]);
                     if(val < min_value){
                         min_value = val;
                         min_k = k;
